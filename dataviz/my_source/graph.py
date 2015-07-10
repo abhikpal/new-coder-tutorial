b7c351feab371a8f217fcf82eede3338ca7b3458
Abhik Pal <abhikpal2509@gmail.com>
2015-07-10 11:52:38 +0530

    Completed Part I of the tutorial
    Started Part II of the dataviz tutorial
    
    Finished half of Part II : completed visualize_days()

---
+++ dataviz/my_source/graph.py
@@ -0,0 +1,46 @@
+from collections import Counter
+import matplotlib.pyplot as plt
+import numpy as numpy
+
+import parse
+
+
+def visualize_days(parsed_data):
+    """
+    Visualizes parsed_data by day of the week.
+    """
+
+    # prepare list that will be used later fow counting.
+    counter = Counter(item["DayOfWeek"] for item in parsed_data)
+
+    # seperate out the counter
+    data_counts = [
+        counter["Monday"],
+        counter["Tuesday"],
+        counter["Wednesday"],
+        counter["Thursday"],
+        counter["Friday"],
+        counter["Saturday"],
+        counter["Sunday"]
+    ]
+
+    # tuple of shorrt days names to be used for labelling the axis
+    day_tuple = tuple(["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"])
+
+    plt.rcParams['toolbar'] = 'None'
+    plt.figure(facecolor='white')
+    # plt.xkcd()
+
+    ## plot the data
+    plt.plot(data_counts)
+
+    ## change the x-ticks
+    plt.xticks(range(len(day_tuple)), day_tuple)
+
+    plt.show()
+
+def main():
+    visualize_days(parse.parse(parse.DATA_FILE, ","))
+
+if __name__ == '__main__':
+    main()
--- dataviz/my_source/parse.py
+++ dataviz/my_source/parse.py
@@ -1,6 +1,7 @@
 import csv
 
-DATA_FILE = "../data/sample_sfpd_incident_all.csv"
+DATA_FILE = "./data/sample_sfpd_incident_all.csv"
+
 
 def parse(raw_file, _delimiter):
     """
@@ -13,14 +14,14 @@ def parse(raw_file, _delimiter):
 
     # open CSV file
     opened_file = open(raw_file)
-    
+
     # read CSV file
     csv_data = csv.reader(opened_file, delimiter=_delimiter)
 
     # build required data structure to return parsed data
     parsed_data = []
 
-    fields = csv_data.next() # the first line is headers
+    fields = csv_data.next()  # the first line is headers
 
     for row in csv_data:
         # fields: [f1, f2, f3, ...]
@@ -32,13 +33,14 @@ def parse(raw_file, _delimiter):
 
     # close CSV file
     opened_file.close()
-    
+
     return parsed_data
+
 
 def main():
     # call the parrser function
     data = parse(DATA_FILE, ',')
-    
+
     # print all of the data
     # (like, why would you do that!?)
     print data
