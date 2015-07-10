49bedc2ff8fb78dab3a1cbee81593e212b11950b
Abhik Pal <abhikpal2509@gmail.com>
2015-07-10 16:55:58 +0530

    Completed dataviz part II: Graph

--- dataviz/my_source/graph.py
+++ dataviz/my_source/graph.py
@@ -1,6 +1,6 @@
 from collections import Counter
 import matplotlib.pyplot as plt
-import numpy as numpy
+import numpy as np
 
 import parse
 
@@ -37,18 +37,59 @@ def visualize_days(parsed_data):
 
     plt.rcParams['toolbar'] = 'None'
     plt.figure(facecolor='white')
-    # plt.xkcd()
 
-    ## plot the data
+    # plot the data
     plt.plot(data_counts)
 
-    ## change the x-ticks
+    # change the x-ticks
     plt.xticks(range(len(day_tuple)), day_tuple)
 
-    plt.show()
+    # plt.show()
+
+    # save the figure
+    plt.savefig("Days.png")
+
+    # close the figure
+    plt.clf()
+
+
+def visualize_type(parsed_data):
+
+    # classify counters by category
+    counter = Counter([item["Category"] for item in parsed_data])
+
+    # set labels for the incidents
+    category_labels = tuple(counter.keys())
+
+    # set starting x locations for the bars
+    x_locations = np.arange(len(category_labels)) + 0.5
+
+    # set width of each bar
+    width = 0.5
+
+    # assign the data to the plot
+    plt.bar(x_locations, counter.values(), width=width)
+
+    # put labels on the graph
+    plt.xticks(x_locations + width/2.0, category_labels, rotation=90)
+
+    # give more space at the bottom to prevent cutting the labels
+    plt.subplots_adjust(bottom=0.4)
+
+    # change figure size
+    plt.rcParams['figure.figsize'] = 12, 8
+
+    # plt.show()
+
+    # save and close
+    plt.savefig('Type.png')
+    plt.clf()
+
 
 def main():
-    visualize_days(parse.parse(parse.DATA_FILE, ","))
+    parsed_data = parse.parse(parse.DATA_FILE, ",")
+    visualize_days(parsed_data)
+    visualize_type(parsed_data)
 
 if __name__ == '__main__':
     main()
